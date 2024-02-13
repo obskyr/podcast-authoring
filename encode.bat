3b90aaf10d9e3736b1f2882d2d5c73de5b2a2028
obskyr <powpowd@gmail.com>
2024-02-13 20:07:27 +0100

    Switched from `.ogg` extension to `.opus` for ideological reasons.
    Ideologically, I support the idea of an open container format – but as it turns out, Ogg is a *terrible* container format that's hindered its own adoption by attempting to be inadvisably generic. I was keeping the `.ogg` extension in order to venerate the veteran container format even now that Opus is on the scene – the codec has enough press, and I didn't want it to steal all the thunder from Ogg – but now that I have some more experience with the specifics of Ogg, I wish for all its thunder to be stolen. Opus deserves a better container format. And there *are* better open container formats! Such as Matroška (or is that Matroska?)! But I can't imagine many podcast apps support `.mka` files!
    
    Either way, ideologically, using the `.opus` extension is… Well, I'm a bit torn, because on the one hand it draws attention away from Ogg (so that it may hopefully one day be superceded by a different open container format), but on the other, it means that `.opus` files will always be Ogg Opus. Although that latter boat has likely already sailed, so I suppose only the former is a factor in any real sense (and even then… the impact this one change is going to have is infinitesimal, hahah).

--- encode.bat
+++ encode.bat
@@ -3,11 +3,11 @@
 @SET SAMPLE_FREQUENCY=44100
 @SET BITRATE=96k
 
-@REM Ogg version. Stereo because libopus for *some reason* makes *larger* mono files than stereo files. 
+@REM Ogg Opus version. Stereo because libopus for *some reason* makes *larger* mono files than stereo files. 
 @REM However, stereo Opus files are still smaller than mono AAC files!!! What a codec!
 @REM https://gitlab.xiph.org/xiph/opus/-/issues/2341
 @REM Worth noting is that Opus does not support 44,100 kHz specifically – but it will correctly round up to 48,000 kHz.
-ffmpeg -i "%~1" -i "%~dp1ffmetadata.txt" -r %SAMPLE_FREQUENCY% -b:a %BITRATE% -vbr off -c:a libopus -map_metadata 0 -map_metadata 1 -y "%~dp1%~n1.ogg"
+ffmpeg -i "%~1" -i "%~dp1ffmetadata.txt" -r %SAMPLE_FREQUENCY% -b:a %BITRATE% -vbr off -c:a libopus -map_metadata 0 -map_metadata 1 -y "%~dp1%~n1.opus"
 
 @REM M4A version. Mono for file size. `-movflags +faststart` puts metadata at the start of the file (as opposed to the end), so that it can be streamed.
 ffmpeg -i "%~1" -i "%~dp1ffmetadata.txt" -ac 1 -r %SAMPLE_FREQUENCY% -b:a %BITRATE% -movflags +faststart -c:a libfdk_aac -map_metadata 0 -map_metadata 1 -y "%~dp1%~n1.m4a"
